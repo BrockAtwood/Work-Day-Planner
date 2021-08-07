ece546c6123607af02a0ac84e81f265a4b9724cc
Brock Atwood <brock.atwood@du.edu>
2021-08-06 23:25:13 -0600

    final functions with localstorage

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 $(init);
 
 function init() {
-  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+  timeBlockColor();
+  setInterval(timeBlockColor, 5000);
+
+  $("#.time-block").each(function () {
+    var noId = $(this).attr("id");
+
+    $("#" + noId + " textarea").text(
+      localStorage.getItem(moment().format("Do, h:mm:ss a'") + noId)
+    );
+  });
+
+  $(".saveBtn").on("click", function () {
+    var timeId = $(this).parent().attr("id");
+    var noteWrote = $(this).siblings(".discription").val();
+    localStorage.setItem(timeId, noteWrote);
+  });
 }
 
+$("#9 .description").val(localStorage.getItem("9"));
+
 function timeBlockColor() {
-  var currentTime = moment().hour();
-  console.log(currentTime);
+  var currentHour = moment().hour();
+  var pastHour = $(this).attr("id)");
 
   $(".time-block").each(function () {
-    var pastHour = parseInt($(this).attr("id").replace("hour-", ""));
+    if (pastHour < currentHour) {
+      $(this).addClass("past");
+    } else if (pastHour == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
   });
 }
