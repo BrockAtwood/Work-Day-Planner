365a2287aa878a3aca6729dff17f6f5259280a2d
Brock Atwood <brock.atwood@du.edu>
2021-08-07 01:55:48 -0600

    fixed style file, changed id in html file, and fixed bug in console and localStorage

--- script.js
+++ script.js
@@ -1,39 +1,166 @@
-$(init);
-
-function init() {
-  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-
-  timeBlockColor();
-  setInterval(timeBlockColor, 5000);
-
-  $("#.time-block").each(function () {
-    var noId = $(this).attr("id");
-
-    $("#" + noId + " textarea").text(
-      localStorage.getItem(moment().format("Do, h:mm:ss a'") + noId)
-    );
-  });
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
   $(".saveBtn").on("click", function () {
-    var timeId = $(this).parent().attr("id");
-    var noteWrote = $(this).siblings(".discription").val();
-    localStorage.setItem(timeId, noteWrote);
+    var noteWrote = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log(time, noteWrote);
+    localStorage.setItem(time, noteWrote);
   });
-}
 
-$("#9 .description").val(localStorage.getItem("9"));
+  $("#9 .description").val(localStorage.getItem("9"));
 
-function timeBlockColor() {
-  var currentHour = moment().hour();
-  var pastHour = $(this).attr("id)");
+  function backgroundColor() {
+    var currentTime = moment().hours();
 
-  $(".time-block").each(function () {
-    if (pastHour < currentHour) {
-      $(this).addClass("past");
-    } else if (pastHour == currentHour) {
-      $(this).addClass("present");
-    } else {
-      $(this).addClass("future");
-    }
-  });
-}
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#10 .description").val(localStorage.getItem("10"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#11 .description").val(localStorage.getItem("11"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#12 .description").val(localStorage.getItem("12"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#13 .description").val(localStorage.getItem("13"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#14 .description").val(localStorage.getItem("14"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#15 .description").val(localStorage.getItem("15"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#16 .description").val(localStorage.getItem("16"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId === currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  $("#17 .description").val(localStorage.getItem("17"));
+
+  function backgroundColor() {
+    var currentTime = moment().hours();
+
+    $(".time-block").each(function () {
+      var timeId = $(this).attr("id");
+
+      if (timeId < currentTime) {
+        $(this).addClass("past");
+      } else if (timeId == currentTime) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  backgroundColor();
+});
