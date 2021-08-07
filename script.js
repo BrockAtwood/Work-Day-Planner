5544e2cbe0caec8f9a5c01b59f141afe9a2d6fb8
Brock Atwood <brock.atwood@du.edu>
2021-08-06 21:30:35 -0600

    add and fix save button

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ $(init);
 function init() {
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 }
+
+function timeBlockColor() {
+  var currentTime = moment().hour();
+  console.log(currentTime);
+
+  $(".time-block").each(function () {
+    var pastHour = parseInt($(this).attr("id").replace("hour-", ""));
+  });
+}
